924cbe1fbf7f7a2d775d3715df68410812788ea7
Nathan Kulczak <nathankulczak@gmail.com>
2020-12-21 00:46:01 -0600

    Python server overhaul
    Switching from asyncio to multithreading
    - asyncio overkill for use case
    - IO boud, quick IO, few connections
    - multithreading is a good solution
    
    CDDPServer
    - main thread accepts connections
    - thread pool to CDDP IO
    - maintain data
    - handle connection and disconnection

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-import asyncio
-import time
-import struct
-import pprint
+
 import functools
-from enum import Enum
+import pprint
+import struct
+import time
+import thread
 
 # import matplotlib.pyplot as plt
 
 
 class CDDPServer:
 
-    def __init__(self, host, port, debug = False):
+    def __init__( self, host, port, debug = False ):
         # parameters
         self.host = host
         self.port = port
@@ -23,87 +23,24 @@ class CDDPServer:
 
     # public methods
 
-    async def start(self):
-        self.server = await asyncio.start_server(
-            self._get_handler(), self.host, self.port
-        )
-
-        self.address = self.server.sockets[0].getsockname()
-
-        async with self.server:
-            await self.server.serve_forever()
-
-        self.connected = True
-        
+    def start( self ):
+        # start server thread
+        print("Starting server thread...")
+        thread.start_new_thread( self._server_thread )
+        print("Server thread started\n")
 
     # private methods
 
-    def _get_handler(self):
+    def _server_thread( self ):
+        # handle new connections
 
-        async def handler(reader, writer):
-
-            print("Handler")
-            
-            # local variables
-            connection_status = 0
-
-            while connection_status != -1:
-
-                print("Awaiting data...")
-
-                # read data
-                data = await self._read_data(reader, writer, 128)
-                data = struct.unpack("IxxxxQ112c", data)
-
-                # parse data
-                # check packet id
-                # parse data based on packet
-                print(f"id:   {data[0]}")
-                print(f"tick: {data[1]}")
-                print(f"data: {data[2]}\n")
-
-                # use data
-
-                # update session
-
-                # create reply
-
-                # write reply
-
-                # await self._write_data(reader, writer, reply_data)
-
-            writer.close()
-
-            # close session
-            current_session.close(save_to_file = True)
-        
-        print("Handler created\n")
-        return handler
+        while True:
+            print("Server thread pass")
+            time.sleep( 10 )
 
 
-    async def _read_data(self, reader, writer, bytes):
-
-        # fetch packet from stream
-        data = await reader.read(bytes)
-
-        return data
-
-
-    async def _write_data(self, reader, writer, reply):
-        writer.write(reply)
-        await writer.drain()
-
-
-async def main():
-
-    # initialize server
-    # server = IPCServer("localhost", 8080, debug = True)
-    server = CDDPServer("localhost", 8989, debug = True)
-
-    print("Server created\n")
-
-    # start server
-    await server.start()
+def main():
+    pass
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
